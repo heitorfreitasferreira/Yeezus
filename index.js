2e06fb9d5b91ac9bdf075feb81a2c0ae9fc124fe
Heitor Freitas <heitor.ff@ufu.br>
2020-08-25 18:41:47 -0300

    Added tracklist and quotes
    I added some quotes from kanye.rest and updated the trackList of the albums, even the new ones, (it's missing the un-released one only)

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ var kanye = {};
 kanye.music = {
     'The College Dropout': {
         albumName: 'The College Dropout',
-        trackList: ["Coming soon.."]
+        trackList: ["Intro (Skit)","We Don't Care","Graduation Day","All Falls Down","I'll Fly Away","Spaceship","Jesus Walks",	"Never Let Me Down","Get Em High",	"Workout Plan (Skit)",	"The New Workout Plan","Slow Jamz","Breathe In Breathe Out" ,	"School Spirit (Skit 1)" ,"School Spirit" ,	"School Spirit (Skit 2)" ,	"Lil Jimmy (Skit)" ,'Two Words" ,	"Through the Wire" ,	"Family Business" , 	"Last Call"  ]
     },
 
     'Late Registration': {
         albumName: 'Late Registration',
-        trackList: ["Coming soon.."]
+        trackList: ["Wake Up Mr. West","Heard 'Em Say"  ,	"Touch the Sky"  ,	"Gold Digger"  ,"Skit No. 1" ,"Drive Slow"  , "My Way Home" ,	"Crack Music"  ,	"Roses" ,	"Bring Me Down" ,	"Addiction" ,	"Skit No. 2", "Diamonds from Sierra Leone (Remix)","We Major" ,	"Skit No. 3" ,	"Hey Mama" ,	"Celebration" ,	"Skit No. 4" ,	"Gone"  ,"Diamonds from Sierra Leone"  ,"Late"   ]
     },
 
     'Graduation': {
         albumName: 'The College Dropout',
-        trackList: ["Coming soon.."]
+        trackList: ["Good Morning",	"Champion" ,"Stronger" ,"I Wonder" ,"Good Life"  ,	"Can't Tell Me Nothing", 	"Barry Bonds" ,"Drunk and Hot Girls" ,"Flashing Lights" ,"Everything I Am"  ,	"The Glory" ,	"Homecoming"  ,	"Big Brother" ]
 
     },
 
     "808's and Heartbreak": {
         albumName: "808's and Heartbreak",
-        trackList: ["Coming soon.."]
+        trackList: ["Say You Will",	"Welcome to Heartbreak" , "Heartless",	"Amazing"  ,	"Love Lockdown" ,	"Paranoid" ,	"RoboCop" ,	"Street Lights" ,"Bad News" ,	"See You in My Nightmares" ,	"Coldest Winter" , 	"Pinocchio Story (Freestyle Live from Singapore)" , , ]
     },
 
     'My Beautiful Dark Twisted Fantasy': {
@@ -30,30 +30,107 @@ kanye.music = {
 
     'Watch the Throne': {
         albumName: 'Watch the Throne',
-        trackList: ["Coming soon.."]
+        trackList: ["No Church in the Wild" , "Lift Off" , 	"Niggas in Paris","Otis" ,	"Gotta Have It" ,	"New Day" ,	"That's My Bitch" ,"Welcome to the Jungle" ,	"Who Gon Stop Me" ,	"Murder to Excellence" ,"Made in America"  ,"Why I Love You" ,	"Illest Motherfucker Alive" ,	"H•A•M" , 	"Primetime","The Joy"]
 
     },
 
     'Yeezus': {
         albumName: 'Yeezus',
-        trackList: ["Coming soon.."]
+        trackList: [	"On Sight" ,"Black Skinhead" ,	"I Am a God" ,	"New Slaves" ,	"Hold My Liquor" , 	"I'm In It",	"Blood on the Leaves" ,	"Guilt Trip" ,	"Send It Up" ,"Bound 2" ]
 
     },
 
     'The Life of Pablo': {
         albumName: 'The Life of Pablo',
-        trackList: ["Coming soon.."]
+        trackList: [	"Ultralight Beam" ,"Father Stretch My Hands, Pt. 1" , 	"Pt. 2",	"Famous" , 	"Feedback",	"Low Lights" ,	"Highlights" ,"Freestyle 4" ,"I Love Kanye" ,	"Waves" ,"FML" ,"Real Friends" ,"Wolves" ,	"Frank's Track" , 	"Siiiiiiiiilver Surffffeeeeer Intermission", "30 Hours",	"No More Parties in LA" ,	"Facts (Charlie Heat Version)" ,	"Fade" ,"Saint Pablo"]
+
+    },
+
+    'Ye': {
+        albumName: 'Ye',
+        trackList: [ 	"I Thought About Killing You", 	"Yikes", 	"All Mine",	"Wouldn't Leave"  ,	"No Mistakes" ,"Ghost Town"  , 	"Violent Crimes"]
+
+    },
+
+    'Jesus Is King': {
+        albumName: 'Jesus Is King',
+        trackList: [	"Every Hour"  ,	"Selah" ,"Follow God" ,	"Closed on Sunday" ,	"On God" ,"Everything We Need"  ,	"Water"  ,	"God Is" , "Hands On" ,"Use This Gospel" ,"Jesus Is Lord" ]
 
     }
 
 
 };
 
-kanye.quotes = [];
+kanye.quotes = [
+	"2024",
+	"All you have to be is yourself",
+	"Believe in your flyness...conquer your shyness.",
+	"Burn that excel spread sheet",
+	"Decentralize",
+	"Distraction is the enemy of vision",
+	"Everything you do in life stems from either fear or love",
+	"For me giving up is way harder than trying.",
+	"For me, money is not my definition of success. Inspiring people is a definition of success",
+	"Fur pillows are hard to actually sleep on",
+	"George Bush doesn't care about black people",
+	"Have you ever thought you were in love with someone but then realized you were just staring in a mirror for 20 minutes?",
+	"I care. I care about everything. Sometimes not giving a f#%k is caring the most.",
+	"I feel calm but energized",
+	"I feel like I'm too busy writing history to read it.",
+	"I feel like me and Taylor might still have sex",
+	"I give up drinking every week",
+	"I leave my emojis bart Simpson color",
+	"I love sleep; it's my favorite.",
+	"I make awesome decisions in bike stores!!!",
+	"I really love my Tesla. I'm in the future. Thank you Elon.",
+	"I still think I am the greatest.",
+	"I think I do myself a disservice by comparing myself to Steve Jobs and Walt Disney and human beings that we've seen before. It should be more like Willy Wonka...and welcome to my chocolate factory.",
+	"I want the world to be better! All I want is positive! All I want is dopeness!",
+	"I wish I had a friend like me",
+	"I'd like to meet with Tim Cook. I got some ideas",
+	"I'll say things that are serious and put them in a joke form so people can enjoy them. We laugh to keep from crying.",
+	"I'm a creative genius",
+	"I'm nice at ping pong",
+	"I'm the best",
+	"If I don't scream, if I don't say something then no one's going to say anything.",
+	"If I got any cooler I would freeze to death",
+	"Just stop lying about shit. Just stop lying.",
+	"Keep squares out yo circle",
+	"Keep your nose out the sky, keep your heart to god, and keep your face to the rising sun.",
+	"Let's be like water",
+	"Man... whatever happened to my antique fish tank?",
+	"My dad got me a drone for Christmas",
+	"My greatest award is what I'm about to do.",
+	"My greatest pain in life is that I will never be able to see myself perform live.",
+	"One day I'm gon' marry a porn star",
+	"One of my favorite of many things about what the Trump hat represents to me is that people can't tell me what to do because I'm black",
+	"Only free thinkers",
+	"People always say that you can't please everybody. I think that's a cop-out. Why not attempt it? Cause think of all the people that you will please if you try.",
+	"People always tell you 'Be humble. Be humble.' When was the last time someone told you to be amazing? Be great! Be awesome! Be awesome!",
+	"People only get jealous when they care.",
+	"Perhaps I should have been more like water today",
+	"Pulling up in the may bike",
+	"Shut the fuck up I will fucking laser you with alien fucking eyes and explode your fucking head",
+	"Sometimes I push the door close button on people running towards the elevator. I just need my own elevator sometimes. My sanctuary.",
+	"Sometimes you have to get rid of everything",
+	"Style is genderless",
+	"The thought police want to suppress freedom of thought",
+	"The world is our family",
+	"The world is our office",
+	"Today is the best day ever and tomorrow's going to be even better",
+	"Truth is my goal. Controversy is my gym. I'll do a hundred reps of controversy for a 6 pack of truth",
+	"Tweeting is legal and also therapeutic",
+	"We all self-conscious. I'm just the first to admit it.",
+	"We came into a broken world. And we're the cleanup crew.",
+	"You can't look at a glass half full or empty if it's overflowing.",
+	"I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle"
+];
 
-
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 kanye.quote = function() {
-    return 'Quotes to be added';
+    return 'this.quote[getRndInteger(0,this.quote.length)]
 };
 
 
